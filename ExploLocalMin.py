5800f89ab2bef3dca309e5047fa0e8bcd14c21f2
Adrien Morison <adrien.morison@gmail.com>
2016-09-22 16:59:25 +0200

    Color of l_star curve depends on l_star value

--- ExploLocalMin.py
+++ ExploLocalMin.py
@@ -5,6 +5,7 @@ Tracking of neutral Ra of harmonic degree corresponding to square cells
 through problems space.
 """
 import numpy as np
+import matplotlib as mpl
 import matplotlib.pyplot as plt
 import seaborn as sns
 from analyzer import LinearAnalyzer
@@ -26,23 +27,50 @@ ana = LinearAnalyzer(
         freeslip_bot=True),
     ncheb=20)
 
-gams = np.linspace(0.2, 0.99, ngamma)
+gams = np.linspace(0.2, 0.85, ngamma)
 
 ran1s = []
 ran2s = []
+l_stars = []
 
 for gamma in gams:
     ana.phys.gamma = gamma
-    (_, ran1), (_, ran2) = ana.ran_l_mins()
+    (_, ran1), (l_star, ran2) = ana.ran_l_mins()
     ran1s.append(ran1 * (1-gamma)**3)
     ran2s.append(ran2 * (1-gamma)**3)
+    l_stars.append(l_star)
+
+l_stars = np.array(l_stars)
+l_vals = np.unique(l_stars)
+ran2s = np.array(ran2s)
+
+# define discrete colormap
+cmap = plt.cm.jet
+lmax = np.max(l_stars)
+norm = mpl.colors.BoundaryNorm(np.append(l_vals, lmax + 1) - 0.5, cmap.N)
+
+# set of line segments colored individually
+points = np.array([gams, ran2s]).T.reshape(-1, 1, 2)
+segments = np.concatenate([points[:-1], points[1:]], axis=1)
+lc = mpl.collections.LineCollection(segments, cmap=cmap, norm=norm)
+lc.set_array(l_stars)
 
 fig, axis = plt.subplots(1, 1)
-axis.plot(gams, ran1s, label=r'$l=1$')
-axis.plot(gams, ran2s, label=r'$l_*$')
+
+axis.plot(gams, ran1s, label=r'$l=1$', linestyle='dotted',
+          color=cmap(norm(1)))
+#axis.plot(gams, ran2s, label=r'$l_\star$')
+cax = axis.add_collection(lc)
+cbar = plt.colorbar(cax, ticks=l_vals)
+cbar.set_label(r'   $l_\star$', rotation=0, fontsize=FTSZ)
+
+#axis.set_xscale('log')
+#axis.set_yscale('log')
+
 axis.set_xlabel(r'$\gamma$', fontsize=FTSZ)
 axis.set_ylabel(r'$\mathrm{Ra}_n(1-\gamma)^3$', fontsize=FTSZ)
 axis.legend(loc='upper right', fontsize=FTSZ)
+
 axis.tick_params(labelsize=FTSZ)
 plt.tight_layout()
 plt.savefig('track_lmin.pdf', format='PDF')
