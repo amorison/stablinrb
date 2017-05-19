df6a412d0d9e13abc96112c87aa37c27e768694a
Adrien Morison <adrien.morison@gmail.com>
2017-05-19 17:06:48 +0200

    Overturn: use solidus as reference T profile

--- CompStability.py
+++ CompStability.py
@@ -3,6 +3,7 @@
 Calculation of stability from an initial composition and temperature profile.
 """
 import numpy as np
+import scipy.integrate as integ
 import matplotlib.pyplot as plt
 import seaborn as sns
 from analyzer import LinearAnalyzer, NonLinearAnalyzer
@@ -14,14 +15,6 @@ from misc import normalize_modes
 FTSZ = 14
 MSIZE = 5
 
-pblm = PhysicalProblem(
-    gamma=None,
-    phi_top=None,
-    phi_bot=1e4,
-    freeslip_top=True,
-    freeslip_bot=True,
-    ref_state_translation=False)
-
 # all in base units
 r_earth = 6371e3
 d_crystal = 1500e3
@@ -29,11 +22,26 @@ r_int = r_earth - d_crystal
 r_ext = lambda h: r_int + h
 rho = 4e3
 g = 10
-alpha = 1e-5
+alpha = 1e-5  # dV/dT /V
+beta = -3.58 / 5.74  # dV/dc /V
 kappa = 1e-6
-delta_temp = lambda h: 3e-4 * h
 tau = lambda s, h: r_ext(h)**2 / (kappa * s)
+# composition
 part = 0.8  # partitioning coefficient
+lam = 0.999  # R_eutectic / R_earth
+c_0 = ((1 - lam**3) * r_earth**3 / (r_earth**3 - r_int**3))**(1 - part)
+# always in case r<R_eutectic
+composition = lambda r, h, c_0=c_0: \
+    c_0 * ((r_earth**3 - r_int**3)
+           / (r_earth**3 - r**3))**(1-part)
+dtmelt_dp = 1e-7
+dtmelt_dc = -1e3
+grad_temp = lambda r, h: -rho * g * dtmelt_dp + \
+    dtmelt_dc * (composition(r, h) * 3 * (1 - part) *
+                 r**2 / (r_earth**3 - r**3))
+delta_temp = lambda h: -integ.quad(
+    lambda r: grad_temp(r, h), r_int, r_ext(h))[0]
+
 eta_vals = np.array(range(15, 19))
 h_crystal_vals = np.linspace(100e3, 1300e3, 50)
 
@@ -41,19 +49,14 @@ h_crystal_vals = np.linspace(100e3, 1300e3, 50)
 gamma = lambda h: r_int / r_ext(h)
 rayleigh = lambda h, eta: \
     rho * g * alpha * delta_temp(h) * r_ext(h)**3 / (eta * kappa)
-ra_comp = 20
-phi_top = 1e4
-phi_bot = 1e4
-# composition
-lam = 0.999  # R_isentrope / R_earth
-c_0 = ((1 - lam**3) * r_earth**3 / (r_earth**3 - r_int**3))**(1 - part)
-composition = lambda r, h, c_0=c_0: \
-    c_0 * (((r_earth**3 - r_int**3) / r_ext(h)**3)
-           / ((r_earth / r_ext(h))**3 - r**3))**(1-part)
+ra_comp = lambda h, eta: \
+    rho * g * beta * r_ext(h)**3 / (eta * kappa)
+phi_top = 1e-2
+phi_bot = 1e-2
 
 ana = LinearAnalyzer(PhysicalProblem(phi_top=phi_top,
                                      phi_bot=phi_bot),
-                     ncheb=50)
+                     ncheb=150)
 
 fig, axis = plt.subplots(1, 1)
 tau_vals = {}
@@ -63,9 +66,13 @@ for eta in eta_vals:
     harm_vals[eta] = []
     for h_crystal in h_crystal_vals:
         ana.phys.gamma = gamma(h_crystal)
-        ana.phys.composition = lambda r: composition(r, h_crystal)
+        ana.phys.composition = lambda r: composition(r * r_ext(h_crystal),
+                                                     h_crystal)
+        ana.phys.grad_ref_temperature = \
+            lambda r: grad_temp(r * r_ext(h_crystal), h_crystal) * \
+                r_ext(h_crystal) / delta_temp(h_crystal)
         sigma, harm = ana.fastest_mode(rayleigh(h_crystal, 10**eta),
-                                       ra_comp)
+                                       ra_comp(h_crystal, 10**eta))
         tau_vals[eta].append(np.real(tau(sigma, h_crystal)))
         harm_vals[eta].append(harm)
     axis.semilogy(h_crystal_vals/1e3, np.array(tau_vals[eta])/1e6,
