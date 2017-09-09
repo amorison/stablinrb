270b1f61d1f80a7d539cdad48efee2379dfa1f5d
Adrien Morison <adrien.morison@gmail.com>
2017-09-09 20:00:31 +0200

    Improve destab time plot
    - Only one viscosity
    - Use right axis and grid for day, year... ticks
    - Add diffusive time

--- CompStability.py
+++ CompStability.py
@@ -70,7 +70,6 @@ delta_temp = lambda h, rm_isen=True: -integ.quad(
 
 h_crystal_max = min(1500e3, r_eut - r_int - 150e3)
 
-eta_vals = np.array(range(16, 18))
 h_crystal_vals = np.logspace(np.log10(5) + 3, np.log10(h_crystal_max), 50)
 phi_vals = [(None, None), (None, 1e-2), (1e-2, 1e-2)]
 
@@ -158,47 +157,47 @@ def plot_destab(ana, crystallized, time):
         ana.phys.phi_bot = phi_bot
         col, phi_str = _phi_col_lbl(phi_top, phi_bot)
 
-        tau_vals = {}
-        harm_vals = {}
-        for eta in eta_vals:
-            harm = 1
-            style = '-' if eta == 17 else '--'
-            tau_vals[eta] = []
-            harm_vals[eta] = []
-            for h_crystal in h_crystal_vals:
-                update_ana_thickness(ana, h_crystal)
-                sigma, harm = ana.fastest_mode(rayleigh(h_crystal, 10**eta),
-                                               ra_comp(h_crystal, 10**eta),
-                                               harm)
-                print(phi_top, phi_bot, eta, sigma, harm, rayleigh(h_crystal, 10**eta))
-                tau_vals[eta].append(np.real(tau(sigma)))
-                harm_vals[eta].append(harm)
-            axt.semilogy(h_crystal_vals/1e3, np.array(tau_vals[eta]),
-                         label=r'$\eta=10^{%d}$, %s' %(eta, phi_str),
-                         color=col, linestyle=style)
-            axl.semilogy(h_crystal_vals/1e3, np.array(harm_vals[eta]),
-                         label=r'$\eta=10^{%d}$, %s' %(eta, phi_str),
-                         color=col, linestyle=style)
+        tau_vals = []
+        harm_vals = []
+        eta = 17
+        harm = 1
+        for h_crystal in h_crystal_vals:
+            update_ana_thickness(ana, h_crystal)
+            sigma, harm = ana.fastest_mode(rayleigh(h_crystal, 10**eta),
+                                           ra_comp(h_crystal, 10**eta),
+                                           harm)
+            print(phi_top, phi_bot, sigma, harm, rayleigh(h_crystal, 10**eta))
+            tau_vals.append(np.real(tau(sigma)))
+            harm_vals.append(harm)
+        axt.semilogy(h_crystal_vals/1e3, tau_vals,
+                     label=phi_str, color=col)
+        axl.semilogy(h_crystal_vals/1e3, harm_vals,
+                     label=phi_str, color=col)
     axt.semilogy(crystallized / 1e3, time, color='k')
-    for duration, name in [(86400, '1d'), (3.15e7, '1y'),
-                           (3.15e10, '1ky'), (3.15e13, '1My')]:
-        axt.semilogy([0, h_crystal_max/1e3], [duration]*2,
-                     color='k', linestyle=':', linewidth=1)
-        axt.text(-h_crystal_max/2e4, duration, name, va='center')
+    axt.semilogy(crystallized[2:] / 1e3, crystallized[2:]**2 / kappa, color='k',
+                 linestyle='--')
+    axt.set_ylim([None, 1e19])
+
+    i_annot = 2 * len(crystallized) // 3
+    axt.annotate('Crystallization time', fontsize=8, va='top',
+                 xy=(crystallized[i_annot] / 1e3, time[i_annot]), rotation=3)
+    axt.annotate('Diffusive time', fontsize=8, va='top',
+                 xy=(crystallized[i_annot]/1e3,
+                     crystallized[i_annot]**2/kappa), rotation=3)
+
+    # more human friendly time units
+    axt_right = axt.twinx()
+    axt_right.set_yscale('log')
+    axt_right.grid(axis='y', ls=':')
+    axt_right.set_ylim(axt.get_ylim())
+    axt_right.set_yticks([86400, 3.15e7, 3.15e10, 3.15e13, 3.15e16])
+    axt_right.set_yticklabels(['1 day', '1 year', '1 kyr', '1 Myr', '1 Gyr'])
+
     axt.set_ylabel(r'Destabilization time scale $\tau$ (s)', fontsize=FTSZ)
     axl.set_ylabel(r'Harmonic degree $l$', fontsize=FTSZ)
     for axis in (axt, axl):
         axis.set_xlabel(r'Crystallized mantle thickness $h$ (km)', fontsize=FTSZ)
-        axis.legend(
-            [plt.Line2D((0,1),(0,0), color='k'),
-             plt.Line2D((0,1),(0,0), color='b', linestyle='--'),
-             plt.Line2D((0,1),(0,0), color='b', linestyle='-'),
-             plt.Line2D((0,1),(0,0), color='b'),
-             plt.Line2D((0,1),(0,0), color='g'),
-             plt.Line2D((0,1),(0,0), color='r')],
-            ['Cooling time', '$\eta=10^{16}$', '$\eta=10^{17}$',
-             'closed', r'$\Phi^+=10^{-2}$', r'$\Phi^+=\Phi^-=10^{-2}$'],
-            loc='upper right', fontsize=FTSZ)
+        axis.legend()
         axis.tick_params(labelsize=FTSZ)
     savefig(figt, 'DestabilizationTimeCryst.pdf')
     savefig(figl, 'DestabilizationTimeCryst_l.pdf')
