63009e978660236e800d78522e414d8ef3f55a33
Adrien Morison <adrien.morison@gmail.com>
2023-02-23 20:10:37 +0000

    packaging: migrate from setup.py to pyproject.toml

--- stablinrb/__init__.py
+++ stablinrb/__init__.py
@@ -1,11 +1,3 @@
 """StabLinRB is a linear stability tool for Rayleigh-Bénard convection."""
 
-from pkg_resources import get_distribution, DistributionNotFound
-from setuptools_scm import get_version
-
-try:
-    __version__ = get_version(root='..', relative_to=__file__)
-except LookupError:
-    __version__ = get_distribution('stablinrb').version
-except (DistributionNotFound, ValueError):
-    __version__ = 'unknown'
+from ._version import __version__, __version_tuple__
