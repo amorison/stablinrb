21afc6bb019142bd3f142ac6dc6b894493a13a3c
Adrien Morison <adrien.morison@gmail.com>
2023-03-09 02:40:18 +0000

    geometry: add RadialOperators ABC and implems
    This will be useful to abstract the definition of the physical
    problem.

--- stablinrb/geometry.py
+++ stablinrb/geometry.py
@@ -1,7 +1,15 @@
 from __future__ import annotations
 
+import typing
 from abc import ABC, abstractmethod
 from dataclasses import dataclass
+from functools import cached_property
+
+import numpy as np
+
+if typing.TYPE_CHECKING:
+    from dmsuite.poly_diff import DiffMatrices
+    from numpy.typing import NDArray
 
 
 class Geometry(ABC):
@@ -17,6 +25,10 @@ class Geometry(ABC):
     def is_spherical(self) -> bool:
         ...
 
+    @abstractmethod
+    def with_dmat(self, diff_mat: DiffMatrices) -> RadialOperators:
+        ...
+
 
 @dataclass(frozen=True)
 class Cartesian(Geometry):
@@ -30,6 +42,9 @@ class Cartesian(Geometry):
 
     def is_spherical(self) -> bool:
         return False
+
+    def with_dmat(self, diff_mat: DiffMatrices) -> CartesianOps:
+        return CartesianOps(diff_mat=diff_mat)
 
 
 @dataclass(frozen=True)
@@ -54,3 +69,85 @@ class Spherical(Geometry):
 
     def is_spherical(self) -> bool:
         return True
+
+    def with_dmat(self, diff_mat: DiffMatrices) -> SphericalOps:
+        return SphericalOps(geom=self, diff_mat=diff_mat)
+
+
+class RadialOperators(ABC):
+    @property
+    @abstractmethod
+    def phys_coord(self) -> NDArray:
+        """Physical radial coordinate."""
+
+    @cached_property
+    @abstractmethod
+    def identity(self) -> NDArray:
+        """Identity operator."""
+
+    @property
+    @abstractmethod
+    def grad_r(self) -> NDArray:
+        """Radial gradient."""
+
+    @property
+    @abstractmethod
+    def lapl_r(self) -> NDArray:
+        """Radial contribution to laplacian."""
+
+
+@dataclass(frozen=True)
+class CartesianOps(RadialOperators):
+    diff_mat: DiffMatrices
+
+    @property
+    def nodes(self) -> NDArray:
+        return self.diff_mat.nodes
+
+    @property
+    def phys_coord(self) -> NDArray:
+        return self.nodes
+
+    @cached_property
+    def identity(self) -> NDArray:
+        return np.identity(self.nodes.size)
+
+    @property
+    def grad_r(self) -> NDArray:
+        return self.diff_mat.at_order(1)
+
+    @property
+    def lapl_r(self) -> NDArray:
+        return self.diff_mat.at_order(2)
+
+
+@dataclass(frozen=True)
+class SphericalOps(RadialOperators):
+    geom: Spherical
+    diff_mat: DiffMatrices
+
+    @property
+    def nodes(self) -> NDArray:
+        return self.diff_mat.nodes
+
+    @cached_property
+    def phys_coord(self) -> NDArray:
+        return self.nodes + self.lambda_
+
+    @cached_property
+    def lambda_(self) -> float:
+        return (2 * self.geom.gamma - 1) / (1 - self.geom.gamma)
+
+    @cached_property
+    def identity(self) -> NDArray:
+        return np.identity(self.nodes.size)
+
+    @property
+    def grad_r(self) -> NDArray:
+        return self.diff_mat.at_order(1)
+
+    @cached_property
+    def lapl_r(self) -> NDArray:
+        two_o_rad = np.diag(2 / self.phys_coord)
+        dr1 = self.diff_mat.at_order(1)
+        return self.diff_mat.at_order(2) + two_o_rad @ dr1
