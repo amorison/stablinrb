e7457883da2d783b8cd7f8f829fd26d759c42802
Adrien Morison <adrien.morison@gmail.com>
2023-03-15 20:44:42 +0000

    Add low phi tests, cartesian and spherical

--- tests/test_linear.py
+++ tests/test_linear.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pytest
 
 import stablinrb.geometry as geom
 import stablinrb.physics as phy
@@ -23,3 +24,48 @@ def test_classic_rb_cart() -> None:
     ra_c, harm_c = ana.critical_ra()
     assert np.isclose(ra_c, ra_th, rtol=1e-4)
     assert np.isclose(harm_c, harm_th, rtol=1e-2)
+
+
+@pytest.mark.parametrize("phi", [1e-3, 3e-2, 1e-2])
+def test_phi_cart(phi: float) -> None:
+    ana = LinearAnalyzer(
+        phys=phy.PhysicalProblem(
+            geometry=geom.Cartesian(),
+            bc_mom_top=phy.PhaseChange(phi),
+            bc_mom_bot=phy.PhaseChange(phi),
+        ),
+        chebyshev_degree=10,
+    )
+
+    # these are only to leading order
+    ra_th = 24 * phi - 81 / 256 * phi**2
+    harm_th = 3 * np.sqrt(phi) / (4 * np.sqrt(2))
+
+    ra_c, harm_c = ana.critical_ra()
+    assert np.isclose(ra_c, ra_th, 1e-4)
+    assert np.isclose(harm_c, harm_th, 5e-2)
+
+
+@pytest.mark.parametrize("gamma", [0.2, 0.4, 0.6, 0.8])
+@pytest.mark.parametrize("phi", [1e-3, 3e-2, 1e-2])
+def test_phi_sph(gamma: float, phi: float) -> None:
+    ana = LinearAnalyzer(
+        phys=phy.PhysicalProblem(
+            geometry=geom.Spherical(gamma),
+            bc_mom_top=phy.PhaseChange(phi),
+            bc_mom_bot=phy.PhaseChange(phi),
+        ),
+        chebyshev_degree=10,
+    )
+
+    ra_th = (
+        24
+        * (phi + gamma**2 * phi)
+        * (1 - gamma**3)
+        / (gamma * (1 - gamma) * (gamma**2 + 4 * gamma + 1))
+    )
+    harm_th = 1
+
+    ra_c, harm_c = ana.critical_ra()
+    assert np.isclose(ra_c, ra_th, 1e-2)
+    assert harm_c == harm_th
