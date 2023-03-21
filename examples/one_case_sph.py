ac1cbc732228aeebf1d653141dd51ba65ccd6cb1
Adrien Morison <adrien.morison@gmail.com>
2023-03-21 02:30:55 +0000

    Add simple example in spherical geometry

---
+++ examples/one_case_sph.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""Find critical Rayleigh number and associated mode.
+
+Minimal example which computes the minimum critical Rayleigh number and the
+associated wavenumber in spherical geometry with free-slip boundaries.
+Additionnally, it plots the critical Ra as function of wavenumber around the
+minimal value and the fastest growing mode.
+"""
+
+import stablinrb.plotting as plotting
+from stablinrb.spherical import SphStability
+
+ana = SphStability(
+    chebyshev_degree=10,
+    gamma=0.5,
+)
+
+# Find the critical Ra and wavenumber and print.
+ra_c, harm_c = ana.critical_ra()
+print("Rac, kc = ", ra_c, harm_c)
+
+plotting.plot_fastest_mode_sph(ana, harm_c, ra_c)
+plotting.plot_ran_harm_sph(ana, harm_c)
